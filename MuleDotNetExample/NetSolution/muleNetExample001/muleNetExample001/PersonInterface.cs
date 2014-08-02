d58e0c101db714b6b63351ddf47f960866c11210
andrewb186 <andrewb186@gmail.com>
2014-08-02 16:23:13 +0200

    started .net solution

---
+++ MuleDotNetExample/NetSolution/muleNetExample001/muleNetExample001/PersonInterface.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using muleNetExample001.objects;
+using muleNetExample001.dbml;
+
+
+namespace muleNetExample001
+{
+    public class PersonInterface
+    {
+        private PersonDataClassesDataContext dataContext;
+
+        public PersonInterface()
+        {
+            dataContext = new PersonDataClassesDataContext();
+        }
+
+        public Person GetPerson(int id)
+        {
+            PersonDetail personInfo = dataContext.PersonDetails.Single(p => p.id == id);
+                        
+            Person person = new Person();
+            person.ID = personInfo.id;
+            person.Name = personInfo.name;
+            person.Surname = personInfo.surname;
+            person.Telephone = personInfo.telephone;
+            person.Email = personInfo.email;
+            person.Age = personInfo.age;
+
+            person.Credential.Username = personInfo.Credential.username;
+            person.Credential.Password = personInfo.Credential.password;
+
+            return person;
+        }
+
+
+    }
+}
