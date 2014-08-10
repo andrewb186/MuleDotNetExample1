e19a2f99d20923b59722b61bb54d5e2dc8f77761
andrewb186 <andrewb186@gmail.com>
2014-08-10 12:39:18 +0200

    java / resource folders

---
+++ MuleDotNetExample/muleSolution/src/main/java/com/mule/net/example/Credentials2.java
@@ -0,0 +1,31 @@
+package com.mule.net.example;
+
+public class Credentials2 {
+	private int personID;
+	private String username;
+	private String password;
+	public int getPersonID() {
+		return personID;
+	}
+	public void setPersonID(int personID) {
+		this.personID = personID;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	public Credentials2(){
+		
+	}
+}
